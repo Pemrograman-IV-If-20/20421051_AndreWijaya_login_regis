bb467343d3ab60a457d0e74df7a1f6bca8a84da2
Andrewij21 <andrewijaya988@gmail.com>
2022-04-29 15:59:20 +0700

    first commit

---
+++ lib/Components/register/registerComponent.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:simple_shadow/simple_shadow.dart';
+import 'package:toko_gitar_flutter/Components/register/registerForm.dart';
+import 'package:toko_gitar_flutter/Utils/constants.dart';
+import 'package:toko_gitar_flutter/size_config.dart';
+
+class registerComponent extends StatefulWidget {
+  @override
+  _registerComponent createState() => _registerComponent();
+}
+
+class _registerComponent extends State<registerComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: getProportionateScreenHeight(20)),
+          child: SingleChildScrollView(
+            child: Column(children: [
+              SizedBox(
+                height: SizeConfig.screenHeight * 0.04,
+              ),
+              SizedBox(
+                height: SizeConfig.screenHeight * 0.04,
+              ),
+              Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("Sign Up",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 30))
+                    ],
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              signUpForm()
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
